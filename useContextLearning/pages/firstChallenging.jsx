46d787c9c9e2614b8ec627328f568726b3d53002
tinymonster123 <lifenghan123@outlook.com>
2023-12-25 11:58:28 +0800

    feat:将onClick作为属性传递给子组件

--- useContextLearning/pages/firstChallenging.jsx
+++ useContextLearning/pages/firstChallenging.jsx
@@ -4,10 +4,10 @@ import { createContext, useState, useContext } from 'react'
 
 const ThemeContext = createContext(null)
 
-// const navigate = useNavigate()
-
 function MyApp() {
     const [theme, setTheme] = useState("light")
+
+
 
     const classname = "box-" + theme
     return (
@@ -15,6 +15,7 @@ function MyApp() {
             <ThemeContext.Provider value={theme}>
                 <div className={classname}>
                     <Form />
+
                 </div>
             </ThemeContext.Provider>
             <label>
@@ -30,12 +31,15 @@ function MyApp() {
 
 
 function Form() {
+
+    const navigate = useNavigate()
+
     return (
         <>
             <Panel title="Welcome">
-                <Button>Sign In</Button>
-                <Button>Log In</Button>
-                {/* <Button onClick = {() => {navigate('/secondChallenge')}}>Next Challenge</Button> */}
+                <Btn>Sign In</Btn>
+                <Btn>Log In</Btn>
+                <Btn onClick={() => { navigate("/CurrentUser") }}>Next Challenging</Btn>
             </Panel>
         </>
     )
@@ -54,12 +58,14 @@ function Panel({ title, children }) {
     )
 }
 
+//  通过onClick（形参）发现了一个很有意思的问题：子组件中的形参其实是一种期望能够从父组件中获得的一种属性。
+//  打个比方：就如同Btn子组件与Form父组件，如果我的Btn中的形参改为click，毫无疑问哦那Click= {onClick}也需改为onClick={click}。但重头戏来了，Form中的onClick={...}也需改为click！我们可以理解为组件中并不能像我们的标签中正常使用如同onClick一般的回调函数，把它们加进去，会将其作为一个属性处理，所以我个人的理解是组件是一种不能使用回调函数的标签，并且其属性要与子组件中的属性保持一致，否则子组件将无法接收属性
 
-function Button({ children }) {
+function Btn({ children, onClick }) {
     const theme = useContext(ThemeContext)
-    const classname = "button-" + theme
+    const classname = "Btn-" + theme
     return (
-        <button className={classname}>{children}</button>
+        <button className={classname} onClick={onClick}>{children}</button>
     )
 }
 
